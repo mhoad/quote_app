50919fa36c89b2f294f8945dc8c5b7cddf171398
Mark Hoad <markdhoad@gmail.com>
2012-12-08 15:57:29 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuoteApp::Application.config.secret_token = '7f6ad5d794abe25434341aba278b0a97037d07a6c5323eec4d695caabab383b0366283f4f0e260a85f5e93575bd942b27a8382d9e52f12c3b6f54ed073db8d99'
